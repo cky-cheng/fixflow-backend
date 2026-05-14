2fe4bfe5289fa88e17ac16cd9051ed9d80479cd0
kory <cky_cheng@sina.com>
2026-05-14 22:18:51 +0800

    框架搭建

---
+++ src/main/java/com/kory/fixflowbackend/FixflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kory.fixflowbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixflowBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kory/fixflowbackend/FixflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kory.fixflowbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
