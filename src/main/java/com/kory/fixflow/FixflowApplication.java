a55bc1fec69499a8287ef9205a86a73fc3a21f7c
kory <cky_cheng@sina.com>
2026-05-14 22:37:22 +0800

    框架搭建

--- src/main/java/com/kory/fixflow/FixflowApplication.java
+++ src/main/java/com/kory/fixflow/FixflowApplication.java
@@ -1,13 +1,13 @@
-package com.kory.fixflowbackend;
+package com.kory.fixflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FixflowBackendApplication {
+public class FixflowApplication {
     
     public static void main(String[] args) {
-        SpringApplication.run(FixflowBackendApplication.class, args);
+        SpringApplication.run(FixflowApplication.class, args);
     }
     
 }
